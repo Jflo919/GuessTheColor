18291c37e420fdf0a9ac7d179126465164b7f506
JulianF <floydjulian1991@gmail.com>
2022-04-26 21:03:12 -0700

    randomness of all colors added

--- script.js
+++ script.js
@@ -10,4 +10,17 @@ function setColor(button, red, green, blue){
 }
 
 // function to randomly choose colors, remember Math.round or floor and Math.random times rgb value
-setColor(buttons[0], 255, 0, 255);
+function makeColorValue() {
+    return Math.round(Math.random() * 255);
+}
+
+// initialize three variables, add for loop here!
+for (var i = 0; i< buttons.length; i++) {
+  
+   let red = makeColorValue();
+   let green = makeColorValue();
+   let blue = makeColorValue();
+
+
+setColor(buttons[i], red, green, blue);
+}
